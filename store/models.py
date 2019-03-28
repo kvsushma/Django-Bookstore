4514d1806273cbb9ba052bcc098add5be3b19e3b
SUSHMA <sushma@sproutssolutions.com>
2019-03-29 00:45:58 +0530

    semi working cart

--- store/models.py
+++ store/models.py
@@ -24,3 +24,17 @@ class Review(models.Model):
 	user=models.ForeignKey(User)
 	publish_date=models.DateField(default=timezone.now)
 	text=models.TextField()
+
+class Cart(models.Model):
+	user=models.ForeignKey(User)
+	active=models.BooleanField(default=True)
+	order_date=models.DateField(null=True)
+	payment_type=models.CharField(max_length=100, null=True)
+	payment_id=models.CharField(max_length=100, null=True)
+
+class BookOrder(models.Model):
+	book=models.ForeignKey(Book)
+	cart=models.ForeignKey(Cart)
+	quantity=models.IntegerField(null=True)
+	payment_type=models.CharField(max_length=100, null=True)
+	payment_id=models.CharField(max_length=100, null=True)
